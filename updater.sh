9615902830c68d41ba89d71f85f665b08f0f36d0
@Kumar <krishnakumar.1905059@srec.ac.in>
2023-03-19 21:38:04 +0530

    Create updater.sh
    initial setup for linux

---
+++ updater.sh
@@ -0,0 +1,12 @@
+echo "-------------------------"
+echo "| UPDATING AND UPGRADING |"
+echo "-------------------------"
+
+# updating and upgrading
+sudo apt update && sudo apt upgrade -y
+sudo apt autoremove
+echo "-------------------------"
+echo "|  UPDATED SUCCESSFULLY  |"
+echo "-------------------------"
+# installing tools
+sudo apt install git htop bpytop -y

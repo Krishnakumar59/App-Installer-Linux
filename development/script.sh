7a1188ca594579df5f22b7b3deef29ac50928624
Krishna Kumar <Krishnakumarjnk@gmail.com>
2023-04-20 18:12:21 +0545

    linux-hotspot added

---
+++ development/script.sh
@@ -0,0 +1,19 @@
+
+
+# python
+
+sudo apt install 
+sudo apt-get install python3 python3-pip python-is-python3
+
+# java
+sudo apt install openjdk-19-jdk openjdk-19-jre
+
+
+# nodejs installation
+sudo apt remove npm nodejs -y
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - &&sudo apt-get install -y nodejs
+
+You can visit [Here] (https://github.com/nodesource/distributions)
+ apt install nodejs
+ echo "Checking Nodejs Version"
+node -v
---
+++ utilities/utils.sh
@@ -0,0 +1,4 @@
+
+# linux wifi-hotspot
+sudo add-apt-repository ppa:lakinduakash/lwh
+sudo apt install linux-wifi-hotspot

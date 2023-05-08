fefdda6b0c66a2602544df76bcc815886099cd05
@Kumar <krishnakumar.1905059@srec.ac.in>
2023-05-08 23:20:37 +0530

    Update utils.sh
    obs studio added

--- utilities/utils.sh
+++ utilities/utils.sh
@@ -2,3 +2,8 @@
 # linux wifi-hotspot
 sudo add-apt-repository ppa:lakinduakash/lwh
 sudo apt install linux-wifi-hotspot
+
+# obs studio
+sudo add-apt-repository ppa:obsproject/obs-studio
+sudo apt update
+sudo apt install ffmpeg obs-studio -y

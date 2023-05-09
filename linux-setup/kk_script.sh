b06e60ea913467df9ee7ae31e24f4d4173177634
@Kumar <krishnakumar.1905059@srec.ac.in>
2023-05-09 23:49:22 +0530

    code rearranged

--- linux-setup/kk_script.sh
+++ linux-setup/kk_script.sh
@@ -1,10 +1,24 @@
+# KK script to setup newly installed ubuntu based linux OS
+
+# upgrading system
 sudo apt update && sudo apt upgrade -y
 
+# installing  programming stuffs
+# python
 sudo apt install -y python3 python3-pip python
+# java
+sudo apt install openjdk-19-jdk openjdk-19-jre
 
+# nodejs
+sudo apt remove npm nodejs -y
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - && sudo apt-get install -y nodejs
+node -v
+
+# installing tools
 sudo apt install -y wget gdebi git curl vlc neofetch gnome-tweaks gnome-shell-extensions telegram-desktop
-sudo apt install synaptic
-sudo apt-get install gnome-sushi
+sudo apt install -y tar tor curl python3 python3-scapy network-manager
+sudo apt install synaptic ncdu -y
+sudo apt-get install gnome-sushi -y
 
 
 # vscode
@@ -30,10 +44,148 @@ sudo dpkg -i zoom_amd64.deb
 sudo apt install -y whois nmap nikto john 
 sudo apt-get install gnome-boxes -y
 
-
+# --- need to fix
 # privacy
-sudo apt install macchanger -y
-sudo apt update && sudo apt install tar tor curl python3 python3-scapy network-manager
-# git clone https://github.com/omer-dogan/kali-whoami
+# git clone https://github.com/Krishnakumar59/whoami-project.git
 # cd kali-whoami/
 # sudo make install
+
+
+# wifi-hotspot qr
+sudo add-apt-repository ppa:lakinduakash/lwh
+sudo apt install linux-wifi-hotspot -y
+
+# downloading OBS STUDIO
+sudo add-apt-repository ppa:obsproject/obs-studio
+sudo apt update
+sudo apt install ffmpeg obs-studio -y
+
+
+# |||||||||||||||||<<<---------DEVOPS--------->>>|||||||||||||||||||
+# DevOps
+# installing devops tools
+sudo apt install git nano wget curl ansible -y
+
+# Installing  Docker Kubernetes and minikube
+
+#Docker and docker-desktop installation
+echo Installing Docker
+
+sudo apt install  ca-certificates  curl  gnupg  lsb-release
+sudo apt install gnome-terminal
+
+# removing old docker package
+
+sudo apt remove docker-desktop
+
+rm -r $HOME/.docker/desktop
+sudo rm /usr/local/bin/com.docker.cli
+sudo apt purge docker-desktop
+
+echo "-------------------------"
+echo "| SETTING UP DOCKER REPO |"
+echo "-------------------------"
+
+#setting up docker repo
+
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg
+    
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
+
+
+echo "----------------------------"
+echo "| INSTALLING DOCKER ENGINE |"
+echo "----------------------------"
+ # Installing docker engine
+  
+ sudo apt-get update
+  
+ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin  
+  
+  
+ #creating docker group
+ sudo groupadd docker
+
+ sudo chown root:docker /var/run/docker.sock
+ 
+ sudo usermod -aG docker $USER
+
+ #  verifying docker is installed or not
+echo "-------------------------------------------"
+echo "|  VERIFYING DOCKER IS INSTALLED OR NOT   |"
+echo "-------------------------------------------"
+
+# running hello-world in docker to check whether it is installed correctly or not
+sudo docker run hello-world
+
+
+# installing docker-desktop
+# downloading docker desktop package
+wget https://desktop.docker.com/linux/main/amd64/docker-desktop-4.18.0-amd64.deb
+
+
+sudo apt install pass uidmap qemu-system-x86 -y
+sudo apt --fix-broken install
+sudo dpkg -i docker-desktop-4.18.0-amd64.deb
+# removing docker file to free up space  
+rm docker-desktop-4.18.0-amd64.deb
+
+echo "----------------------------------------------"
+echo "|  DOCKER-DESKTOP IS INSTALLED  SUCCESSFULLY  |  |"
+echo "----------------------------------------------"
+
+
+echo "----------------------------------------------"
+echo "|       MINIKUBE INSTALLTION IN-PROGRESS      |"
+echo "----------------------------------------------"
+
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+minikube start
+kubectl get po -A
+alias kubectl="minikube kubectl --"
+minikube dashboard
+
+
+############### JENKINS INSTALLATION ################
+
+# installing jenkins
+sudo apt update
+sudo apt install -y fontconfig openjdk-11-jre
+java -version
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
+
+
+sudo systemctl enable jenkins
+
+sudo systemctl start jenkins
+
+
+echo "Download jenkins war file and run"
+# to run jenkins download jenkins.war file from jenkins site or you can download from this repo
+# wget https://github.com/Krishnakumar59/App-Installer-Linux/blame/a9aeb0b54572226e2f9dfcfeffdaec994ccd48ac/devops/jenkins.war
+# java -jar jenkins.war --httpPort=8081
+# open browser and type this
+# http://localhost:8081
+# you will see administrator password in terminal scroll above 

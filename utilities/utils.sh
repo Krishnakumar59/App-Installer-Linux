3850059cede6e5674ca31673fbcfb1d6083d6a7c
@Kumar <krishnakumar.1905059@srec.ac.in>
2023-05-11 20:27:33 +0530

    Update utils.sh

--- utilities/utils.sh
+++ utilities/utils.sh
@@ -7,3 +7,15 @@ sudo apt install linux-wifi-hotspot
 sudo add-apt-repository ppa:obsproject/obs-studio
 sudo apt update
 sudo apt install ffmpeg obs-studio -y
+
+# warp cli installation
+sudo apt install gnupg2 libnss3-tools
+wget https://pkg.cloudflareclient.com/uploads/cloudflare_warp_2023_3_398_1_amd64_ddd2a223f7.deb
+
+sudo dpkg -i cloudflare_warp_2023_3_398_1_amd64_ddd2a223f7.deb
+# register first before use read/mark all agreements 
+warp-cli register 
+# to connect 
+warp-cli connect
+
+

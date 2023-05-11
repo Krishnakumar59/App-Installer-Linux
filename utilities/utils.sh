63ef5a5a09ee904989929fd832741ebbd7276858
@Kumar <krishnakumar.1905059@srec.ac.in>
2023-05-11 20:39:53 +0530

    Update utils.sh

--- utilities/utils.sh
+++ utilities/utils.sh
@@ -8,6 +8,13 @@ sudo add-apt-repository ppa:obsproject/obs-studio
 sudo apt update
 sudo apt install ffmpeg obs-studio -y
 
+
+# sublime installation
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
+
 # warp cli installation
 sudo apt install gnupg2 libnss3-tools
 wget https://pkg.cloudflareclient.com/uploads/cloudflare_warp_2023_3_398_1_amd64_ddd2a223f7.deb

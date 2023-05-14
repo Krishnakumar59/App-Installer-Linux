a691fa8e88904d71b4f16374c9fd5eb4362e3210
@Kumar <krishnakumar.1905059@srec.ac.in>
2023-05-15 02:53:17 +0530

    fix and updated

--- linux-setup/kk_script.sh
+++ linux-setup/kk_script.sh
@@ -29,7 +29,7 @@ sudo systemctl start jenkins
 
 # installing  programming stuffs
 # python
-sudo apt install -y python3 python3-pip python
+sudo apt install -y python3 python3-pip
 
 # Jetbrain Toolbox Installation
 wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.28.1.15219.tar.gz
@@ -42,7 +42,7 @@ chmod +x jetbrains-toolbox
 
 
 # java
-sudo apt install openjdk-19-jdk openjdk-19-jre
+sudo apt install openjdk-19-jdk openjdk-19-jre -y
 
 # nodejs
 sudo apt remove npm nodejs -y
@@ -56,7 +56,8 @@ sudo apt install synaptic ncdu -y
 sudo apt-get install gnome-sushi -y
 # Text-Extractor - OCR powered screen-capture tool to capture information instead of images 
 # Link: https://github.com/dynobo/normcap
-sudo apt install build-essential tesseract-ocr tesseract-ocr-eng libtesseract-dev libleptonica-dev wl-clipboard
+sudo apt install build-essential tesseract-ocr tesseract-ocr-eng libtesseract-dev libleptonica-dev wl-clipboard -y
+sudo apt install python3-pip -y
 pip install normcap
 # you can set keyboard shortcut as of your choice
 
@@ -70,6 +71,8 @@ sudo rm code_1.77.3-1681292746_amd64.deb
 sudo apt install libu2f-udev -y
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt --fix-broken install
+
 # sudo rm google-chrome-stable_current_amd64.deb
 
 # Zoom 
@@ -77,12 +80,14 @@ sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt install -y libglib2.0-0 libgstreamer-plugins-base0.10-0 libxcb-shape0 libxcb-shm0 libxcb-xfixes0 libxcb-randr0 libxcb-image0 libfontconfig1 libgl1-mesa-glx libxi6 libsm6 libxrender1 libpulse0 libxcomposite1 libxslt1.1 libsqlite3-0 libxcb-keysyms1 libxcb-xtest0 ibus
 wget https://cdn.zoom.us/prod/5.14.5.2430/zoom_amd64.deb
 sudo dpkg -i zoom_amd64.deb
+sudo apt --fix-broken install
 # remove zoom to free space
 # sudo rm zoom_amd64.deb
 
 # kali
 sudo apt install -y whois nmap nikto john 
 sudo apt-get install gnome-boxes -y
+sudo apt --fix-broken install
 
 # --- need to fix
 # privacy
@@ -94,11 +99,13 @@ sudo apt-get install gnome-boxes -y
 # wifi-hotspot qr
 sudo add-apt-repository ppa:lakinduakash/lwh
 sudo apt install linux-wifi-hotspot -y
+sudo apt --fix-broken install
 
 # downloading OBS STUDIO
 sudo add-apt-repository ppa:obsproject/obs-studio
 sudo apt update
 sudo apt install ffmpeg obs-studio -y
+sudo apt --fix-broken install
 
 
 # |||||||||||||||||<<<---------DEVOPS--------->>>|||||||||||||||||||
@@ -150,17 +157,18 @@ echo "| INSTALLING DOCKER ENGINE |"
 echo "----------------------------"
  # Installing docker engine
   
- sudo apt-get update
+sudo apt-get update
   
- sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin  
-  
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin  
+sudo apt --fix-broken install
+
   
  #creating docker group
- sudo groupadd docker
+sudo groupadd docker
 
- sudo chown root:docker /var/run/docker.sock
+sudo chown root:docker /var/run/docker.sock
  
- sudo usermod -aG docker $USER
+sudo usermod -aG docker $USER
 
  #  verifying docker is installed or not
 echo "-------------------------------------------"
@@ -173,14 +181,17 @@ sudo docker run hello-world
 
 # installing docker-desktop
 # downloading docker desktop package
-wget https://desktop.docker.com/linux/main/amd64/docker-desktop-4.18.0-amd64.deb
+wget https://desktop.docker.com/linux/main/amd64/docker-desktop-4.19.0-amd64.deb
 
 
 sudo apt install pass uidmap qemu-system-x86 -y
 sudo apt --fix-broken install
-sudo dpkg -i docker-desktop-4.18.0-amd64.deb
+sudo dpkg -i docker-desktop-4.19.0-amd64.deb
+sudo apt --fix-broken install
 # removing docker file to free up space  
-rm docker-desktop-4.18.0-amd64.deb
+rm docker-desktop-4.19.0-amd64.deb
+
+
 
 echo "----------------------------------------------"
 echo "|  DOCKER-DESKTOP IS INSTALLED  SUCCESSFULLY  |  |"
